5a024389cc8c13367a435d8a6e08e150e06693b1
dominika dzida <dominika.dzida@edu.uekat.pl>
2023-11-13 12:19:06 +0100

    dodanie nowego pliku

---
+++ python_zad4/zad_1.py
@@ -0,0 +1,19 @@
+#ćw 1
+
+class Student:
+    def __init__(self,name,marks ) -> None:
+        self.name = name
+        self.marks = marks
+
+    def is_passed(self):
+        average_marks = sum(self.marks) / len(self.marks)
+        return average_marks > 50
+
+Student1 = Student("Marek", [60,70,80])
+Student2 = Student("Jacek", [10,30,20])
+
+wynik1 = Student1.is_passed()
+print(f"Czy {Student1.name} zdał/a? {wynik1}")
+
+wynik2= Student2.is_passed()
+print(f"Czy {Student2.name} zdał/a? {wynik2}")
---
+++ python_zad4/zad_3.py
@@ -0,0 +1,32 @@
+class Property:
+    def __init__(self, area, rooms, price, address):
+        self.area = area
+        self.rooms = rooms
+        self.price = price
+        self.address = address
+
+    def __str__(self):
+        return f"Property: {self.address}, {self.rooms} rooms, {self.area} sq. meters, ${self.price}"
+
+class House(Property):
+    def __init__(self, area, rooms, price, address, plot):
+        super().__init__(area, rooms, price, address)
+        self.plot = plot
+
+    def __str__(self):
+        return f"House: {self.address}, {self.rooms} rooms, {self.area} sq. meters, ${self.price}, Plot: {self.plot} sq. meters"
+
+class Flat(Property):
+    def __init__(self, area, rooms, price, address, floor):
+        super().__init__(area, rooms, price, address)
+        self.floor = floor
+
+    def __str__(self):
+        return f"Flat: {self.address}, {self.rooms} rooms, {self.area} sq. meters, ${self.price}, Floor: {self.floor}"
+
+house = House(area=150, rooms=4, price=200000, address="123 Main Street", plot=500)
+
+flat = Flat(area=80, rooms=2, price=120000, address="456 Elm Street", floor=3)
+
+print(house)
+print(flat)
