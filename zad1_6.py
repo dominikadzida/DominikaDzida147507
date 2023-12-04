d5dee1fa0adeb2563d9e91448916d9d5a06559e6
dominika dzida <dominika.dzida@edu.uekat.pl>
2023-12-04 12:53:06 +0100

    pep 8

--- zad1_6.py
+++ zad1_6.py
@@ -1,32 +1,38 @@
-# -*- coding: utf-8 -*-
+#  pycodestyle --exclude=venv .-*- coding: utf-8 -*-
 """
 Spyder Editor
 
 This is a temporary script file.
 """
 
-#Stworzyć funkcję, która przyjmuje 2 argumenty (typu string ) name i surname , a
-#następnie zwróci stringa zgodnie ze wzorem Cześć {name} {surname}! Należy
-#Operatory arytmetyczne, logiczne i typowanie 3
-#uruchomić funkcję, wynik wykonania funkcji zapisać do zmiennej, a następnie go
-#wyświetlić ( print )
+# Stworzyć funkcję, która przyjmuje 2 argumenty
+# (typu string ) name i surname , a
+# następnie zwróci stringa zgodnie ze
+# wzorem Cześć {name} {surname}! Należy
+# Operatory arytmetyczne, logiczne i typowanie 3
+# uruchomić funkcję, wynik wykonania funkcji
+# zapisać do zmiennej, a następnie go
+# wyświetlić ( print )
 
 
 def create_greeting(name, surname):
     greeting = f"Cześć {name} {surname}!"
     return greeting
 
+
 name = "John"
-surname= "Deer"
+surname = "Deer"
 result = create_greeting(name, surname)
 print(result)
 
 
-#Stworzyć funkcję, która przyjmie 2 argumenty typu int , a następnie zwróci wynik
-#mnożenia obu liczb.
+# Stworzyć funkcję, która przyjmie 2 argumenty
+# typu int , a następnie zwróci wynik
+# mnożenia obu liczb.
 
 def pomnoz(a, b):
     return a * b
+
 
 # Przykładowe użycie funkcji
 liczba1 = 5
@@ -34,17 +40,23 @@ liczba2 = 3
 wynik = pomnoz(liczba1, liczba2)
 print(wynik)
 
-#Stworzyć funkcję, która będzie sprawdzała czy przekazana liczba w parametrze jest
-#parzysta i zwróci tą informację jako typ logiczny bool ( True / False ). Należy
-#uruchomić funkcję, wynik wykonania zapisać do zmiennej, a następnie
-#wykorzystując warunek logiczny wyświetlić prawidłowy tekst "Liczba parzysta" /
-#"Liczba nieparzysta"
+# Stworzyć funkcję, która będzie
+# sprawdzała czy przekazana liczba w parametrze jest
+# parzysta i zwróci tą informację jako typ
+# logiczny bool ( True / False ). Należy
+# uruchomić funkcję, wynik wykonania zapisać
+# do zmiennej, a następnie
+# wykorzystując warunek logiczny wyświetlić
+# prawidłowy tekst "Liczba parzysta" /
+# "Liczba nieparzysta"
+
 
 def czy_parzysta(liczba):
     if liczba % 2 == 0:
         return True
     else:
         return False
+
 
 # Przykładowe użycie funkcji
 liczba = 7
@@ -55,12 +67,16 @@ if wynik:
 else:
     print("Liczba nieparzysta")
 
-#Stworzyć funkcję, która przyjmie 3 argumenty typu int i sprawdzi czy suma dwóch
-#pierwszych liczb jest większa lub równa trzeciej, a następnie zwróci tę informację
-#jako typ logiczny bool
+# Stworzyć funkcję, która przyjmie 3 argumenty typu
+# int i sprawdzi czy suma dwóch
+# pierwszych liczb jest większa lub równa trzeciej, a
+# następnie zwróci tę informację
+# jako typ logiczny bool
+
 
 def sprawdz_suma(a, b, c):
     return a + b >= c
+
 
 # Przykładowe użycie funkcji
 liczba1 = 5
@@ -75,14 +91,17 @@ else:
     print("Suma dwóch pierwszych liczb jest mniejsza niż trzecia.")
 
 
-
-#Stworzyć funkcję, która przyjmie 2 argumenty. Pierwszy typu list , a drugi typu int
-# Funkcja ma sprawdzić (zwracając typ logiczny bool ), czy lista z parametru
-#pierwszego zawiera taką wartość jaką przekazano w parametrze drugim.
+# Stworzyć funkcję, która przyjmie 2 argumenty.
+# Pierwszy typu list , a drugi typu int
+# Funkcja ma sprawdzić (zwracając typ logiczny bool ),
+# czy lista z parametru
+# pierwszego zawiera taką wartość jaką
+# przekazano w parametrze drugim.
 
 
 def sprawdz_wartosc_w_liscie(lista, wartosc):
     return wartosc in lista
+
 
 # Przykładowe użycie funkcji
 moja_lista = [1, 2, 3, 4, 5]
@@ -96,10 +115,12 @@ else:
     print(f"Lista nie zawiera wartości {szukana_wartosc}.")
 
 
-
-#Stworzyć funkcję, która przyjmuje 2 argumenty typu list i zwraca wynik typu list .
-#Funkcja ma za zadanie złączyć przekazane listy w jedną, usunąć duplikaty, każdy
-#element podnieść do potęgi 3 stopnia, a następnie zwrócić powstałą listę.
+# Stworzyć funkcję, która przyjmuje 2 argumenty
+# typu list i zwraca wynik typu list .
+# Funkcja ma za zadanie złączyć przekazane
+# listy w jedną, usunąć duplikaty, każdy
+# element podnieść do potęgi 3 stopnia,
+# a następnie zwrócić powstałą listę.
 
 def przetworz_listy(lista1, lista2):
 
@@ -108,8 +129,8 @@ def przetworz_listy(lista1, lista2):
     unikalna_lista = list(set(polaczona_lista))
 
     przetworzona_lista = [x**3 for x in unikalna_lista]
-    
     return przetworzona_lista
+
 
 # Przykładowe użycie funkcji
 lista1 = [1, 2, 3, 4]
@@ -117,16 +138,3 @@ lista2 = [3, 4, 5, 6]
 
 wynik = przetworz_listy(lista1, lista2)
 print(wynik)
-
-
-
-
-
-
-
-
-
-
-
-
- 
